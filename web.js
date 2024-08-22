999363734617445b0c7230d6f7ebb417fed4547e
choiminwoo98 <cmw0918@naver.com>
2024-08-23 08:28:38 +0900

    8002 포트변경

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = 8001;
+const PORT = 8002;
 
 app.use(express.static(path.join(__dirname, "build")));
 
