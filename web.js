a9495945e2b1b5bbb96b080fbf4f51f2868a0046
choiminwoo98 <cmw0918@naver.com>
2024-08-29 10:53:07 +0900

    다시

--- web.js
+++ web.js
@@ -3,20 +3,10 @@ const app = express();
 const path = require("path");
 const PORT = 8001;
 
-// www.sbvaforum.com에서 접속했을 때의 처리
-app.use((req, res, next) => {
-  if (req.headers.host === "www.sbvaforum.com") {
-    // 이 부분에서 원하는 처리를 할 수 있습니다.
-    // 예를 들어, 특정 파일을 보내거나 다른 라우터로 연결 가능
-    return res.sendFile(path.join(__dirname, "build", "index.html"));
-  }
-  next();
-});
-
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(__dirname + "/build/index.html");
 });
 
 app.listen(PORT, () => {
