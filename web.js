7546fb5c31aa6dba444a10d7e70acff7d89ad28c
choiminwoo98 <cmw0918@naver.com>
2024-08-29 10:41:51 +0900

    www로 올경우 www 없애기

--- web.js
+++ web.js
@@ -3,6 +3,15 @@ const app = express();
 const path = require("path");
 const PORT = 8001;
 
+// www 접속을 비-www로 리다이렉트하는 미들웨어
+app.use((req, res, next) => {
+  if (req.headers.host.slice(0, 4) === "www.") {
+    const newHost = req.headers.host.slice(4);
+    return res.redirect(301, req.protocol + "://" + newHost + req.originalUrl);
+  }
+  next();
+});
+
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/", function (req, res) {
